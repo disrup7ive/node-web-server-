8b3f1929c09be5aae4170ddcfb5d5bb6f5933c7b
disrup7ive <disrup7ive@gmail.com>
2019-01-02 10:55:58 +0000

    Added project page.

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
+
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
